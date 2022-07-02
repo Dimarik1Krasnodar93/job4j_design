dadf83265b55359030fcc7a6688e88d37a59a168
Dimarik <krasnodar93@mail.ru>
2022-07-02 17:37:16 +0300

    0207 EvenNumbersIteratorTest

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -7,7 +7,8 @@ import java.util.Optional;
 public class EvenNumbersIterator implements Iterator<Integer> {
 
     private int[] data;
-    private int index = 0;
+    private int index = -1;
+    private int indexHasNext = -1;
 
     public EvenNumbersIterator(int[] data) {
         this.data = data;
@@ -16,9 +17,10 @@ public class EvenNumbersIterator implements Iterator<Integer> {
     @Override
     public boolean hasNext() {
         boolean rsl = false;
-        for (int i = index; i < data.length; i++) {
+        for (int i = indexHasNext + 1; i < data.length; i++) {
             if (data[i] % 2 == 0) {
                 rsl = true;
+                indexHasNext = i;
                 break;
             }
         }
@@ -27,14 +29,17 @@ public class EvenNumbersIterator implements Iterator<Integer> {
 
     @Override
     public Integer next() {
+        indexHasNext = index;
         if (!hasNext()) {
             throw new NoSuchElementException();
         } else {
-            for (int i = index; i < data.length; i++) {
+            for (int i = index + 1; i < data.length; i++) {
                 if (data[i] % 2 == 0) {
                     index = i;
                     Optional optPointer = Optional.of(data[index]);
                     if (optPointer.isPresent()) {
+                        index = i;
+                        indexHasNext = i;
                         return data[index];
                     } else {
                         return next();
