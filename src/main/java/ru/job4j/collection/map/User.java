ce4f10bb25e19f29a573da738b5d218cb0283415
Dimarik <krasnodar93@mail.ru>
2022-07-14 09:55:37 +0300

    1407 1. Создать модель User [#999]

---
+++ src/main/java/ru/job4j/collection/map/User.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection.map;
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private Calendar birthday;
+
+    public User(String name, Calendar birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+}
