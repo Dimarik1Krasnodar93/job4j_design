86c24c8a706e5c17299664a5df6b72b36dd5d037
Dimarik <krasnodar93@mail.ru>
2022-07-25 15:30:54 +0300

    2507 - выброс исключений

--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.tree;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertThrows;
 import static org.hamcrest.Matchers.is;
 
 
@@ -38,6 +40,15 @@ public class SimpleTreeTest {
         tree.add(1, 4);
         assertThat(tree.isBinary()).isFalse();
     }
+
+    @Test
+    void when6ElFindLastThen6Null() {
+        Tree<Integer> tree = new SimpleTree<>(2);
+        NoSuchElementException thrown = Assertions.assertThrows(NoSuchElementException.class, () -> {
+            tree.add(1, 2);
+        });
+    }
+
 
     @Test
     void whenElFindNotExistThenOptionEmpty() {
