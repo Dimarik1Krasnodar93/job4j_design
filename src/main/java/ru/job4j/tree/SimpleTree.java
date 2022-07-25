840529d1136fb837a39e158e0cb5ad21888dbb67
Dimarik <krasnodar93@mail.ru>
2022-07-25 11:27:57 +0300

    2507 -tree - 2 lambda 2

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -26,23 +26,23 @@ public class SimpleTree<E> implements Tree<E> {
     @Override
     public Optional<Node<E>> findBy(E value) {
         Predicate<Node<E>> pred = s -> s.value.equals(value);
-        Optional<Node<E>> rsl = binaryFind(pred);
+        Optional<Node<E>> rsl = findByPredicate(pred);
         return rsl;
     }
 
     public boolean isBinary() {
         Predicate<Node<E>> pred = s -> s.children.size() > 2;
-        Optional<Node<E>> rsl = binaryFind(pred);
+        Optional<Node<E>> rsl = findByPredicate(pred);
         return !rsl.isPresent();
     }
 
-    private Optional<Node<E>> binaryFind(Predicate<Node<E>> pred) {
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (pred.test(el)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
