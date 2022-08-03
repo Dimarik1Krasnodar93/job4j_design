fdd0c933f8cb8118bc2aaf2a402e6172355a17c9
Dimarik <krasnodar93@mail.ru>
2022-08-03 09:15:44 +0300

    0.1. FileOutputStream. [#252488]3

---
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -0,0 +1,17 @@
+package ru.job4j.io;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("input.txt")){
+            StringBuilder text = new StringBuilder();
+            int read;
+            while((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
