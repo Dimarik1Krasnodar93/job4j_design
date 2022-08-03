60e11d6261c342c804a0a26195271275de472d69
Dimarik <krasnodar93@mail.ru>
2022-08-03 09:19:42 +0300

    0.1. FileOutputStream. [#252488]4

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 
 public class ReadFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("input.txt")){
+        try (FileInputStream in = new FileInputStream("input.txt")) {
             StringBuilder text = new StringBuilder();
             int read;
-            while((read = in.read()) != -1) {
+            while ((read = in.read()) != -1) {
                 text.append((char) read);
             }
         } catch (IOException e) {
