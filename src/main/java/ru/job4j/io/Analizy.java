ea9e65c7f526c639c28319ccb5c95ac849fb64a9
Dimarik <krasnodar93@mail.ru>
2022-08-16 16:27:07 +0300

    2. Анализ доступности сервера. [#859]-2

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -5,75 +5,38 @@ import java.util.*;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        Deque<StartEnd> deque = new LinkedList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+            PrintWriter out = new PrintWriter(
+                    new BufferedOutputStream(
+                            new FileOutputStream(target)));
             String temp = in.readLine();
-            StartEnd startEnd;
             String[] strSplit;
+            boolean isError = false;
             while (temp != null) {
-                startEnd = deque.pollLast();
                 strSplit = temp.split(" ");
-                if (startEnd == null || startEnd.end != null) {
-                    if (strSplit[0].equals("400") || strSplit[0].equals("500")) {
-                        if (startEnd != null) {
-                            deque.add(startEnd);
-                        }
-                        startEnd = new StartEnd(strSplit[1]);
+                if (strSplit[0].equals("400") || strSplit[0].equals("500")) {
+                    if (!isError) {
+                        isError = true;
+                        out.print(strSplit[1]);
+                        System.out.print(strSplit[1]);
                     }
                 } else {
-                   if (!strSplit[0].equals("400") && !strSplit[0].equals("500")) {
-                       startEnd.end = strSplit[1];
-                   }
-                }
-                if (startEnd != null) {
-                    deque.add(startEnd);
+                    if (isError) {
+                        out.println(";" + strSplit[1]);
+                        System.out.println(";" + strSplit[1]);
+                        isError = false;
+                    }
                 }
                 temp = in.readLine();
             }
+            out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)))) {
-            for (StartEnd startEnd : deque) {
-                out.println(startEnd.start + ";" + (startEnd.end == null ? "" : startEnd.end));
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 
     public static void main(String[] args) {
         Analizy analizy = new Analizy();
         analizy.unavailable("unavailable.csv", "target.csv");
-    }
-
-    public class StartEnd {
-        private String start;
-        private String end;
-
-        public StartEnd(String start) {
-            this.start = start;
-        }
-
-        public String getStart() {
-            return start;
-        }
-
-        public void setStart(String start) {
-            this.start = start;
-        }
-
-        public String getEnd() {
-            return end;
-        }
-
-        public void setEnd(String end) {
-            this.end = end;
-        }
-
     }
 }
