be7a159368c7d97cb17586b4cb6541bf848aa080
Dimarik <krasnodar93@mail.ru>
2022-08-16 16:53:23 +0300

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,15 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("pom.xml");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            System.out.println(file.getName() + ": size: " + file.length());
+        }
+    }
+}
