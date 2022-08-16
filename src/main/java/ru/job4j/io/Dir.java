05914e8c89dfe01c674251912e2c5f0664cfc29a
Dimarik <krasnodar93@mail.ru>
2022-08-16 22:09:51 +0300

    Dir

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -9,7 +9,7 @@ public class Dir {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
-            System.out.println(String.format(file.getName() + ": size: %s", file.length()));
+            System.out.println(String.format("name: %s; size: %d", file.getName(), file.length()));
         } else {
             for (File subfile : file.listFiles()) {
                 System.out.println(subfile.getAbsoluteFile());
