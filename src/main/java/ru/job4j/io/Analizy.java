8486b172e64fd75e219b07eeb0795c781efe77b9
Dimarik <krasnodar93@mail.ru>
2022-08-16 21:59:24 +0300

    2. Анализ доступности сервера. [#859 #332843]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -5,31 +5,28 @@ import java.util.*;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            PrintWriter out = new PrintWriter(
-                    new BufferedOutputStream(
-                            new FileOutputStream(target)));
-            String temp = in.readLine();
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(
+                     new BufferedOutputStream(
+                             new FileOutputStream(target)))) {
+            String temp = "";
             String[] strSplit;
             boolean isError = false;
-            while (temp != null) {
+            while (in.ready()) {
+                temp = in.readLine();
                 strSplit = temp.split(" ");
-                if (strSplit[0].equals("400") || strSplit[0].equals("500")) {
+                if ("400".equals(strSplit[0]) || "500".equals(strSplit[0])) {
                     if (!isError) {
                         isError = true;
                         out.print(strSplit[1]);
-                        System.out.print(strSplit[1]);
                     }
                 } else {
                     if (isError) {
                         out.println(";" + strSplit[1]);
-                        System.out.println(";" + strSplit[1]);
                         isError = false;
                     }
                 }
-                temp = in.readLine();
             }
-            out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
