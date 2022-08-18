e27df115d1830f4114634760db3e4a00ae1b4308
Dimarik <krasnodar93@mail.ru>
2022-08-18 10:56:12 +0300

    Dir

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("pom.xml");
+        File file = new File("data");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
@@ -12,7 +12,8 @@ public class Dir {
             System.out.println(String.format("name: %s; size: %d", file.getName(), file.length()));
         } else {
             for (File subfile : file.listFiles()) {
-                System.out.println(subfile.getAbsoluteFile());
+                System.out.println(String.format("name: %s; size: %d",
+                        subfile.getAbsoluteFile().getName(), subfile.getAbsoluteFile().length()));
             }
         }
     }
