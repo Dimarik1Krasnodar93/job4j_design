efd98aba78cbb18c222269c517b098dbbcd47112
Dimarik <krasnodar93@mail.ru>
2022-08-20 10:13:00 +0300

    Подключение библиотеки AssertJ [#504881 #332910]-2

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -36,6 +36,6 @@ public class ListUtils {
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        removeIf(list, t-> elements.contains(t));
+        removeIf(list, t -> elements.contains(t));
     }
 }
