643fe6e8ba1b4e6f40d6491b384b06632203c998
Dimarik <krasnodar93@mail.ru>
2022-08-23 11:53:26 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,17 +1,30 @@
 package ru.job4j.io;
 
 import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
 
-    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        double height = 174.5d;
+        LOG.debug("User info height : {}", height);
+        float weight = 79.55f;
+        LOG.debug("User info weight : {}", weight);
+        boolean isMale = true;
+        LOG.debug("User is male : {}", isMale);
+        String city = "Moscow";
+        LOG.debug("User's city : {}", city);
+        String country = "Russia";
+        LOG.debug("User's city : {}", country);
+        String education = "engineer";
+        LOG.debug("User's education : {}", education);
+        String educationCountry = "Russia";
+        LOG.debug("User's education country : {}", educationCountry);
     }
 }
