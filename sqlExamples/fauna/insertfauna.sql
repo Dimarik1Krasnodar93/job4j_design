e49fe6fa4bc49c632fba07d940b30136f0c1d6e5
Dimarik <krasnodar93@mail.ru>
2022-09-01 10:27:20 +0300

    0. Select [#6861 #338274] 01 09 2022

---
+++ sqlExamples/fauna/insertfauna.sql
@@ -0,0 +1,7 @@
+insert into fauna (name, avg_age, discovery_date) values ('fish', 15, date '2020-01-01');
+insert into fauna (name, avg_age, discovery_date) values ('cat', 15, date '2010-01-01');
+insert into fauna (name, avg_age, discovery_date) values ('fishShark', 15000, date '2000-01-01');
+insert into fauna (name, avg_age, discovery_date) values ('Forel fish', 30, date '2011-02-04');
+insert into fauna (name, avg_age) values ('crocodile', 200);
+insert into fauna (name, avg_age, discovery_date) values ('Dog', 15, date '1911-02-04');
+insert into fauna (name, avg_age, discovery_date) values ('Dingo', 17, date '1711-02-04');
