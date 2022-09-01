82590853d84b4d50a4e206a301a5c3bd51e087b6
Dimarik <krasnodar93@mail.ru>
2022-09-01 16:44:59 +0300

    1. Inner join [#373617] 01 09 2022 - 2

--- sqlExamples/persons/allinOneScript.sql
+++ sqlExamples/persons/allinOneScript.sql
@@ -18,11 +18,14 @@ insert into persons (passport_id, name) values (1, 'Ivanov');
 insert into persons (passport_id, name) values (2, 'Petrov');
 insert into employeersPersons (name, person_id)  values ('грузчик', 1);
 insert into employeersPersons (name, person_id)  values ('водитель', 1);
-select * from passport join persons on passport.id = persons.passport_id join employeersPersons on persons.id = employeersPersons.person_id;
+select * from passport join persons on passport.id = persons.passport_id
+join employeersPersons on persons.id = employeersPersons.person_id;
 select pas.number as Номер, pers.name Имя, emp.name Должность
-from passport pas JOIN persons pers on pas.id = pers.passport_id join employeersPersons emp on pers.id = emp.person_id;
+from passport pas JOIN persons pers on pas.id = pers.passport_id
+join employeersPersons emp on pers.id = emp.person_id;
 select pas.number as Номер, pers.name Имя, emp.name Должность
-from passport pas JOIN persons pers on pas.id = pers.passport_id join employeersPersons emp on pers.id = emp.person_id;
+from passport pas JOIN persons pers on pas.id = pers.passport_id
+join employeersPersons emp on pers.id = emp.person_id;
 select pas.number as "Номер паспорта", pas.id id, pers.name ФИО, emp.name Должность
 from passport pas join persons pers on pas.id = pers.passport_id
 join employeersPersons emp on pers.id = emp.person_id;
