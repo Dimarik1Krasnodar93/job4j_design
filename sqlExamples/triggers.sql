c5cae3d09399ca13eee66f81b2a6a3168842e5e8
Dimarik <krasnodar93@mail.ru>
2022-09-09 18:43:03 +0300

    триггеры 09 09 2022 -2

--- sqlExamples/triggers.sql
+++ sqlExamples/triggers.sql
@@ -1,16 +1,10 @@
-create table products (
-    id serial primary key,
-    name varchar(50),
-    producer varchar(50),
-    count integer default 0,
-    price integer
-);
 
 create or replace function taxAfter()
 	returns trigger as
 	$$
 		BEGIN
-			new.price = new.price * 1.2
+		update products
+			set price = price * 1.2
 			where id = (select id from inserted);
 			return NEW;
 		END;
@@ -62,3 +56,4 @@ create trigger copy_prices_after
 	on products
 	for each row
 	execute procedure copy_prices();
+
