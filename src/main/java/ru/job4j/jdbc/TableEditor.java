fb5a603a46c6c98d671162356d4e2f5a26a3232f
Dimarik <krasnodar93@mail.ru>
2022-09-18 19:56:46 +0300

    Statement 1809

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -13,10 +13,7 @@ public class TableEditor implements AutoCloseable {
     private Connection connection;
 
     private Properties properties;
-
-    public TableEditor() {
-
-    }
+    private Statement st;
 
     public TableEditor(Properties properties) {
         this.properties = properties;
@@ -24,27 +21,26 @@ public class TableEditor implements AutoCloseable {
     }
 
     private void initConnection() {
-        Properties prs = loadProperties();
+        loadProperties();
         try {
-            connection = DriverManager.getConnection(prs.get("url").toString(), prs);
+            connection = DriverManager.getConnection(properties.get("url").toString(), properties);
+            st = connection.createStatement();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
     }
 
-    private Properties loadProperties() {
-        Properties prs = new Properties();
+    private void loadProperties() {
         final ClassLoader classLoader = TableEditor.class.getClassLoader();
         try (InputStream io = classLoader.getResourceAsStream("app.properties")) {
-            prs.load(io);
+            properties.load(io);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        return prs;
     }
 
     public void createTable(String tableName) {
-        try (Statement st = connection.createStatement()) {
+        try {
             var res = st.execute(String.format("create table %s (id serial primary key)", tableName));
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -52,7 +48,7 @@ public class TableEditor implements AutoCloseable {
     }
 
     public void dropTable(String tableName) {
-        try (Statement st = connection.createStatement()) {
+        try {
             var res = st.executeUpdate(String.format("drop table %s", tableName));
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -60,7 +56,7 @@ public class TableEditor implements AutoCloseable {
     }
 
     public void addColumn(String tableName, String columnName, String type) {
-        try (Statement st = connection.createStatement()) {
+        try {
             var res = st.executeUpdate(String.format("alter table %s add column %s %s", tableName, columnName, type));
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -68,7 +64,7 @@ public class TableEditor implements AutoCloseable {
     }
 
     public void dropColumn(String tableName, String columnName) {
-        try (Statement st = connection.createStatement()) {
+        try {
             var res = st.executeUpdate(String.format("alter table %s drop column %s", tableName, columnName));
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -76,7 +72,7 @@ public class TableEditor implements AutoCloseable {
     }
 
     public void renameColumn(String tableName, String columnName, String newColumnName) {
-        try (Statement st = connection.createStatement()) {
+        try {
             var res = st.executeUpdate(String.format("alter table %s rename %s to %s", tableName, columnName, newColumnName));
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -110,18 +106,22 @@ public class TableEditor implements AutoCloseable {
     }
 
     public static void main(String[] args) throws Exception {
-        TableEditor te = new TableEditor();
-        te.initConnection();
-        String tableName = "t" + System.currentTimeMillis();
-        te.createTable(tableName);
-        System.out.println(getTableScheme(te.connection, tableName));
-        te.addColumn(tableName, "name", "text");
-        System.out.println(getTableScheme(te.connection, tableName));
-        te.renameColumn(tableName, "name", "newname");
-        System.out.println(getTableScheme(te.connection, tableName));
-        te.dropColumn(tableName, "newname");
-        System.out.println(getTableScheme(te.connection, tableName));
-        te.dropTable(tableName);
-        System.out.println(getTableScheme(te.connection, tableName));
+        Properties prs = new Properties();
+        try (TableEditor te = new TableEditor(prs)) {
+            te.initConnection();
+            String tableName = "t" + System.currentTimeMillis();
+            te.createTable(tableName);
+            System.out.println(getTableScheme(te.connection, tableName));
+            te.addColumn(tableName, "name", "text");
+            System.out.println(getTableScheme(te.connection, tableName));
+            te.renameColumn(tableName, "name", "newname");
+            System.out.println(getTableScheme(te.connection, tableName));
+            te.dropColumn(tableName, "newname");
+            System.out.println(getTableScheme(te.connection, tableName));
+            te.dropTable(tableName);
+            System.out.println(getTableScheme(te.connection, tableName));
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
