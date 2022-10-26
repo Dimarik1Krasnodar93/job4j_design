1803534b587d861609d07524a52ab2f2f3e38f76
Dimarik <krasnodar93@mail.ru>
2022-10-26 10:45:42 +0300

    принцип зависимостей

--- src/main/java/ru/job4j/ood/dip/Car.java
+++ src/main/java/ru/job4j/ood/dip/Car.java
@@ -3,7 +3,7 @@ package ru.job4j.ood.dip;
 import java.util.HashMap;
 import java.util.Map;
 
-public abstract class Car extends Bus{
+public abstract class Car extends Bus {
     protected int year;
     protected HashMap<Detail, Integer> map;
 }
