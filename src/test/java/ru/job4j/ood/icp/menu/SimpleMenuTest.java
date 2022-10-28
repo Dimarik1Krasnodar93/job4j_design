bae92807dd06b0ff97c8d919e40a65c1bf9ee2cd
Dimarik <krasnodar93@mail.ru>
2022-10-28 22:45:28 +0300

    вывод отчетов

--- src/test/java/ru/job4j/ood/icp/menu/SimpleMenuTest.java
+++ src/test/java/ru/job4j/ood/icp/menu/SimpleMenuTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.ood.icp.menu;
 
+import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.ood.icp.menu.ActionDelegate;
 import ru.job4j.ood.icp.menu.SimpleMenu;
@@ -12,6 +14,7 @@ public class SimpleMenuTest {
 
     public static final ActionDelegate STUB_ACTION = System.out::println;
 
+    @Disabled
     @Test
     public void whenAddThenReturnSame() {
         Menu menu = new SimpleMenu();
@@ -20,6 +23,17 @@ public class SimpleMenuTest {
         menu.add("Сходить в магазин", "Купить продукты", STUB_ACTION);
         menu.add("Купить продукты", "Купить хлеб", STUB_ACTION);
         menu.add("Купить продукты", "Купить молоко", STUB_ACTION);
+        assertThat(new Menu.MenuItemInfo("Сходить в магазин",
+                List.of("Купить продукты"), STUB_ACTION, "1."))
+                .isEqualTo(menu.select("Сходить в магазин").get());
+        assertThat(new Menu.MenuItemInfo(
+                "Купить продукты",
+                List.of("Купить хлеб", "Купить молоко"), STUB_ACTION, "1.1."))
+                .isEqualTo(menu.select("Купить продукты").get());
+        assertThat(new Menu.MenuItemInfo(
+                "Покормить собаку", List.of(), STUB_ACTION, "2."))
+                .isEqualTo(menu.select("Покормить собаку").get());
+        menu.forEach(i -> System.out.println(i.getNumber() + i.getName()));
     }
 
 }
