7ea46d4f57aa3725887ea93facff8ab676df7c54
Dimarik <krasnodar93@mail.ru>
2022-11-18 18:18:25 +0300

    1. Хранилище продуктов [#852 #355330]

--- src/main/java/ru/job4j/ood/lsp/store/Quality.java
+++ src/main/java/ru/job4j/ood/lsp/store/Quality.java
@@ -2,10 +2,11 @@ package ru.job4j.ood.lsp.store;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class Quality {
     public static double getPercentExpiry(LocalDateTime createDate, LocalDateTime expiryDate) {
-        return 100 - 100 * (Timestamp.valueOf(expiryDate).getTime() - System.currentTimeMillis())
-                / (Timestamp.valueOf(expiryDate).getTime() - Timestamp.valueOf(createDate).getTime());
+        return (DAYS.between(expiryDate, LocalDateTime.now()) * 100.00)
+                / DAYS.between(expiryDate, createDate);
     }
 }
